cb7b0c9d1b8442f93b99cf23a2c32d22e64160df
Luis0248 <139874713+Luis0248@users.noreply.github.com>
2026-02-25 15:44:57 -0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,10 +1,12 @@
 import schemathesis
 from schemathesis.checks import not_a_server_error
+from hypothesis import settings
 
 schema = schemathesis.openapi.from_url("https://petstore.swagger.io/v2/swagger.json")
 
 @schema.parametrize()
+# Reducimos a solo 5 ejemplos por cada método para que sea rápido
+@settings(max_examples=5) 
 def test_api_metodos(case):
     response = case.call()
-    
     case.validate_response(response, checks=(not_a_server_error,))
